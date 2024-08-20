d117d533a76338bb3fcc679b142005d0adb7dc08
ShimeiYago <harrisonhightower4@gmail.com>
2024-08-20 21:28:47 +0900

    tensorflow example

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM tensorflow/tensorflow:2.16.2
 
 RUN apt-get update && apt-get install -y python3-pip
 
@@ -13,6 +13,14 @@ COPY app.py ${APP_DIR}
 
 COPY ./entry_script.sh /entry_script.sh
 ADD ./aws-lambda-rie /usr/bin/aws-lambda-rie
+
+# ここから
+ARG MODEL_DIR="${APP_DIR}/models"
+RUN mkdir -p ${MODEL_DIR}
+COPY models/vgg16_weights_tf_dim_ordering_tf_kernels.h5 ${MODEL_DIR}
+COPY models/imagenet_class_index.json ${MODEL_DIR}
+ENV TF_CPP_MIN_LOG_LEVEL="2"
+# ここまで追加
 
 WORKDIR ${APP_DIR}
 
--- app.py
+++ app.py
@@ -1,8 +1,46 @@
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras import preprocessing
+import numpy as np
+import base64
+import PIL
+import io
 import json
 
 def handler(event, context):
-    res = {
-        "message": "Hello World"
-    }
+    # 学習済みモデルをロード
+    model = VGG16(weights='models/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+
+    # リクエストBodyから入力画像(base64形式)を取得
+    input_image_base64 = event["inputImageBase64"]
     
+    # 入力画像をPIL形式に変換
+    img = base64_to_pil(input_image_base64)
+
+    # サイズをVGG16のデフォルトである224x224にリサイズ
+    img = img.resize((224, 224))
+
+    # 読み込んだPIL形式の画像をarrayに変換
+    x = preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    
+    # 予測
+    preds = model(preprocess_input(x))
+    
+    # 最も高い確率のクラス名を取得
+    max_index = np.argmax(preds)
+    with open("models/imagenet_class_index.json") as f:
+        class_index = json.load(f)
+        class_name = class_index[str(max_index)][1]
+    
+    # レスポンス
+    res = {
+        "class": class_name
+    }
     return res
+
+
+# base64画像文字列をデコードしてPIL画像に変換
+def base64_to_pil(img_base64):
+    img_data = base64.b64decode(img_base64)
+    img = PIL.Image.open(io.BytesIO(img_data))
+    return img
